49367336fd6627f987c94ba81cfa1fe685033f56
mrkielo <kielas35@gmail.com>
2025-09-01 22:29:04 +0200

    input system WIP

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,52 @@
+using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] List<Rigidbody2D> rigidbodies;
+    [SerializeField] float mSpeed;
+    InputSystem_Actions inputActions;
+
     void Start()
     {
-        
+        inputActions = new InputSystem_Actions();
+        inputActions.UI.Disable();
+        inputActions.Player.Enable();
+
+        inputActions.Player.Move.performed += Move;
+        inputActions.Player.Move.canceled += Move;
+
+        inputActions.Player.Jump.performed += Jump;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void Move(InputAction.CallbackContext ctx)
+    {
+        Vector2 dir = ctx.action.ReadValue<Vector2>();
+        Debug.Log(dir);
+        foreach (var rb in rigidbodies)
+        {
+            rb.linearVelocityX = dir.x * mSpeed;
+        }
+
+    }
+
+    void Jump(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
 }
